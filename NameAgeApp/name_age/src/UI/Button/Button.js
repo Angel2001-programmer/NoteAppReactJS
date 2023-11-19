d997dbad2537c8e3c90cdad6512db865a05f6511
Angel <62680147+Angel2001-programmer@users.noreply.github.com>
2023-11-19 16:20:15 +0000

    Delete NameAgeApp/name_age directory

--- NameAgeApp/name_age/src/UI/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import styled from './Button.module.css';
-
-const Button = props => {
-    return(
-        <button onClick={props.clickHandler} type={props.type} className={styled.button}>{props.text}</button>
-    );
-}
-
-export default Button;
